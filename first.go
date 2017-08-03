88fcf1418549791ef9029c97ef5f4380a5ac6815
Your Name <you@example.com>
2017-08-04 01:10:03 +0800

    go some test

---
+++ first.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+var a = "菜鸟教程"
+var b string = "runoob.com"
+var c bool
+
+
+
+func main() {
+    fmt.Println("Hello, World!")
+	fmt.Println("Hello, World!")
+	fmt.Println("w3cschool菜鸟教程：w3cschool.cc")
+	
+	println(a, b, c)
+	
+}
